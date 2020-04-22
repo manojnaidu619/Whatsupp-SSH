ba3034479a9925cd871cc53699da554da57e672c
Manoj Naidu <manojnaidu619@gmail.com>
2020-04-22 12:22:31 +0530

    Updated packeage.json

--- src/app.js
+++ src/app.js
@@ -1,40 +1,29 @@
 const express = require("express")
 const bodyParser = require('body-parser')
-const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
 const masterProcess = require("./utils/masterProcess")
 const requestLogger = require("./logger/requestLogger")
 const cdTracker = require("./utils/cdTracker")
+const twilio = require("./utils/twilio")
 
 const app = express()
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 app.use(requestLogger)
+
+const PORT = 3000
 console.log("initial Load")
 
 // Listening for requests in /SMSsh
 app.post('/SMSsh', (req, res) => { 
-    const twiml = new MessagingResponse();
-    command = req.body.Body.toString()
+    command = req.body.Body.toString()  
     cdTracker()
         .then(cwd => {
             masterProcess(command, cwd)
-                .then(data => {
-                    twiml.message(data)
-                    res.writeHead(200, {'Content-Type': 'text/xml'});
-                    res.end(twiml.toString());
-                })
-                .catch(err => {
-                    twiml.message(err)
-                    res.writeHead(200, {'Content-Type': 'text/xml'});
-                    res.end(twiml.toString());
-                })
+                .then(data => twilio(data, res))
+                .catch(err => twilio(err, res))
         })
-        .catch(err => {
-            twiml.message(err)
-            res.writeHead(200, {'Content-Type': 'text/xml'});
-            res.end(twiml.toString());
-        })
+        .catch(err => twilio(err, res))
 })
 
-app.listen(3000)
+app.listen(PORT, () => console.log(`Listening on port : ${PORT}`))
--- src/utils/twilio.js
+++ src/utils/twilio.js
@@ -1,18 +1,10 @@
-const http = require('http');
-const express = require('express');
 const MessagingResponse = require('twilio').twiml.MessagingResponse;
 
-const app = express();
-
-app.post('/sms', (req, res) => {
+const twilio = (data, res) => {
   const twiml = new MessagingResponse();
-
-  twiml.message('The Robots are coming! Head for the hills!');
-
+  twiml.message(data)
   res.writeHead(200, {'Content-Type': 'text/xml'});
   res.end(twiml.toString());
-});
+}
 
-http.createServer(app).listen(1337, () => {
-  console.log('Express server listening on port 1337');
-});
+module.exports = twilio
