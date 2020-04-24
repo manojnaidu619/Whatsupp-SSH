8cc29d9ce00f23ccc603baf3eabf7f5e83c04898
Manoj Naidu <manojnaidu619@gmail.com>
2020-04-24 10:21:06 +0530

    Fixed initial login issue

--- src/utils/authValidator.js
+++ src/utils/authValidator.js
@@ -1,28 +1,35 @@
 const fs = require('fs')
 const path = require('path')
 const twilio = require('./twilio')
-const authTime = 60000             // in milliseconds
+
+const authTime = 20000             // in milliseconds
+const filePath = path.join(__dirname, "..", "authTime.txt")
+const sshPassword = process.env.SSH_PSWD.toString().trim()
+global.initialLogin = true
+
+const writeDataToFile = (data, res) => {
+    fs.writeFile(filePath, data, (err) => { if (err) twilio(err, res) })
+}
 
 const authValidator = (req, res, next) => {
     const reqData = req.body.Body
-    const sshPassword = process.env.SSH_PSWD.toString().trim()
 
     new Promise((resolve, reject) => {
-        fs.access(path.join(__dirname, "..", "authTime.txt"), fs.F_OK, (err) => {
+        fs.access(filePath, fs.F_OK, (err) => {
             if (err) {
-                fs.writeFileSync(path.join(__dirname, "..", "authTime.txt"), Date.now(), (err) => { if (err) reject(err) })
+                writeDataToFile(Date.now(), res)
             }
             resolve(null)
         })
     }).then(() => { 
-        console.log("Exists!")
-        fs.readFile(path.join(__dirname, "..", "authTime.txt"), (err, data) => { 
+        fs.readFile(filePath, (err, data) => { 
             const lastAuthTime = parseInt(data.toString().trim())
-            if ((Date.now() - lastAuthTime) > authTime) {
+            if ((Date.now() - lastAuthTime) > authTime || initialLogin) {
                 if (reqData != sshPassword) {
                     twilio("Please authenticate...", res); return
                 }
-                fs.writeFileSync(path.join(__dirname, "..", "authTime.txt"), Date.now(), (err) => { if (err) reject(err) })
+                writeDataToFile(Date.now(), res)
+                initialLogin = false
                 twilio("Authenticated!", res)
             } else {
                 next()
